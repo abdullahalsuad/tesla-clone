97f35581616f5474a58647e233c18e5c0e8b1f19
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2021-05-19 17:44:31 +0600

    first commit

---
+++ src/components/Section.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Section() {
+    return (
+        <Wrap>
+            section
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+
+        width: 100vw;
+        height: 100vh;
+        background: red;
+
+
+`
